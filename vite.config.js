e625c7f8f6f996ee10d5e5eed63a8d2d235ce3ce
Carlos <carlos206819@gmail.com>
2025-03-29 18:11:41 -0300

    Add docker compose

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Permite que outros containers acessem
+        port: 5173, // Porta do Vite
+        strictPort: true, // Garante que a porta não mude
+        hmr: {
+            host: 'localhost' // Isso pode precisar ser alterado para o nome do serviço no Docker
+        }
+    },
     plugins: [
         laravel({
             input: [
