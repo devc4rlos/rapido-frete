bd6a73b48edb9a195e6e1890e7ef413137b9331a
Carlos <carlos206819@gmail.com>
2025-04-09 15:25:14 -0300

    Refactoring pages fleet with atomic design

--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -3,9 +3,7 @@
 @section('title', 'Nossa frota')
 
 @section('single')
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-        @for($i = 1; $i < 10; $i++)
-            <img src="{{ asset('assets/gallery/' . $i . '.png') }}" alt="" class="w-full h-96 object-cover object-top rounded-xl border border-default-100 animate">
-        @endfor
-    </div>
+    <x-molecules.gallery
+        :images="array_map(fn($i) => asset('assets/gallery/'. $i .'.png'), range(1, 9))"
+    />
 @endsection
