b08db1a9b568685feee8d0970fb532950470ec73
Carlos <carlos206819@gmail.com>
2025-04-09 19:57:15 -0300

    Fix layer message warning

--- resources/js/components/message-warning.js
+++ resources/js/components/message-warning.js
@@ -1,8 +1,55 @@
+import anime from "animejs";
+
 const messagesWarning = document.querySelectorAll('.message-warning');
+const messageWarningContent = document.getElementById('messageWarningContent');
+const containerMessage = document.getElementById("containerMessageWarning")
 
 messagesWarning.forEach(message => {
-    message.addEventListener('click', (e) => {
-        e.target.closest("#containerMessageWarning").classList.toggle('active');
+    message.addEventListener('click', () => {
+        if(containerMessage.classList.contains('progress')){
+            return;
+        }
+
+        if (containerMessage.classList.contains('active')) {
+            hideMessageWarning();
+            return;
+        }
+
+        activeMessageWarning();
     })
 })
 
+function hideMessageWarning() {
+    containerMessage.classList.add('progress');
+    anime({
+        targets: messageWarningContent,
+        opacity: [1, 0],
+        translateX: ['-100%'],
+        duration: 500,
+        easing: "easeOutQuad",
+        complete: () => {
+            containerMessage.classList.remove('progress');
+            messageWarningContent.classList.add('hidden');
+        }
+    });
+
+    containerMessage.classList.remove('active');
+}
+
+function activeMessageWarning() {
+    messageWarningContent.classList.remove('hidden');
+
+    containerMessage.classList.add('progress');
+    anime({
+        targets: messageWarningContent,
+        opacity: [0, 1],
+        translateX: [0],
+        duration: 500,
+        easing: "easeOutQuad",
+        complete: () => {
+            containerMessage.classList.remove('progress');
+        }
+    });
+
+    containerMessage.classList.add('active');
+}
--- resources/views/components/organisms/message-warning.blade.php
+++ resources/views/components/organisms/message-warning.blade.php
@@ -1,19 +1,19 @@
 <section
     @class([
-        'bottom-28 left-0 fixed z-40 flex flex-col items-start group/message',
+        'top-2/4 fixed z-40 flex flex-col items-start group/message active',
         $class,
     ])
     id="containerMessageWarning"
 {{ $attributes }}>
     <div class="bg-info-400 h-14 px-5 flex items-center justify-between gap-x-3 message-warning cursor-pointer relative">
         <x-icons.triangle-exclamation size="lg" class="text-warning-100" />
-        <x-atoms.text class="text-light text-nowrap group-[.active]/message:hidden">Site fictício</x-atoms.text>
+        <x-atoms.text class="text-light text-nowrap hidden group-[.active]/message:block">Site fictício</x-atoms.text>
 
-        <div class="w-6 h-6 flex border border-info-100 items-center justify-center bg-info-300 text-light rounded-full absolute -top-3 -right-3 group-[.active]/message:hidden">
+        <div class="w-6 h-6 hidden border border-info-100 items-center justify-center bg-info-300 text-light rounded-full absolute -top-3 -right-3 group-[.active]/message:flex">
             <x-icons.close />
         </div>
     </div>
-    <div class="overflow-hidden w-full shadow-depth group-[.active]/message:-translate-x-full group-[.active]/message:opacity-0 transition-all duration-700">
+    <div class="overflow-hidden w-full shadow-depth" id="messageWarningContent">
         <x-atoms.text class="overflow-hidden shadow-lg bg-light w-full sm:max-w-md lg:max-w-2xl p-5">
             Este é um site fictício, desenvolvido exclusivamente para fins de demostração no portfólio de <a href="https://carlosalexandre.com.br/" target="_blank" class="text-blue-700">Carlos Alexandre</a>. Os serviços exibidos são apenas demonstrativos e não estão disponíveis para contratação.
         </x-atoms.text>
